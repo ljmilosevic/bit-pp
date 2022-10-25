3774417f7cb6665c497a017df9452f59d6b43f7f
ljmilosevic <114806362+ljmilosevic@users.noreply.github.com>
2022-10-25 08:30:35 +0200

    Add files via upload

--- variable/index.js
+++ variable/index.js
@@ -1,1 +1,113 @@
+//1.
+var a = 5;
+var b = "pet";
+var c = true;
+var d;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+//2.
+var numOne = 4;
+var numTwo = 3;
+var res = numOne + numTwo;
+console.log(res);
+
+res = numOne - numTwo;
+console.log(res);
+
+res = numOne * numTwo;
+console.log(res);
+
+res= numOne / numTwo;
+console.log(res);
+
+//3.
+var f,j,h,d;
+f = 3;
+j = 2;
+h = 31;
+var res2 = f - j - h;
+console.log(res2);
+
+j = 5;
+h = 7;
+d = 3;
+res2 = f * j - h / d;
+console.log(res2);
+
+f = 4;
+j = 2;
+h = 1.14;
+d = 3;
+res2= f / j - h * d;
+console.log(res2);
+
+//.4
+var oneKgGram = 1000;
+var bananaWeight = 1.2;
+var gramWeightBananas = oneKgGram * bananaWeight;
+console.log(gramWeightBananas + "gr");
+
+//.5
+var oneMinSec, anaLate, tomLate, extraSec;
+oneMinSec = 60;
+anaLate = 5;
+tomLate = 30;
+
+extraSec = oneMinSec * anaLate;
+console.log(extraSec + "s");
+
+extraSec = oneMinSec * tomLate ;
+console.log(extraSec + "s");
+
+//.6
+//1GB = 1000MB
+//4GB = 4000MB
+//1MB = 1000KB
+//4000MB = 4000000KB
+//4000000KB / 98KB = 40000KB
+var oneGb = 1000;
+var additionalMemory = oneGb * 4;
+console.log(additionalMemory + "MB");
+
+var forThousandMb = 4000000;
+var newsArticlesEachSize = 98;
+var newsArticles = forThousandMb / newsArticlesEachSize;
+console.log(newsArticles + "KB");
+
+//.7
+a = a * 2;
+a *= 2;
+b = b + 3;
+b += 3;
+c = c / 5;
+c /= 5;
+d = d - 7;
+d -= 7;
+
+//.8
+var myName, age, desc;
+myName = "Ljiljana";
+age = 36;
+desc = "Ljiljana nema mačku";
+console.log(myName);
+console.log(age);
+console.log(desc);
+
+//.9
+console.log("number");
+console.log(true);
+console.log(null);
+console.log("false");
+console.log (56);
+
+//.10
+typeof variable === "boolean";
+
+
+
+
+
+
 
